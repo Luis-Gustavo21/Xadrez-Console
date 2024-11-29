8393a8b059ffb4be5c9fa02506b2bf4325e92a1a
Luis <luis.gustavo.167@outlook.com>
2024-11-29 04:19:50 -0300

    Implementando a logica de xequemate

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -62,8 +62,15 @@ namespace xadrez
             {
                 xeque = false;
             }
-            turno++;
-            mudaJogador();
+            if (testeXequemate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
+            else
+            {
+                turno++;
+                mudaJogador();
+            }
         }
         public void validarPosicaoDeOrigem(Posicao pos)
         {
@@ -162,6 +169,36 @@ namespace xadrez
             }
             return false;
         }
+        public bool testeXequemate(Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+            foreach(Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for (int i = 0; i < tab.linhas; i++)
+                {
+                    for (int j = 0; j < tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao origem = x.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
         public void colocarPecaNova(char coluna, int linha, Peca peca)
         {
             tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
@@ -170,18 +207,11 @@ namespace xadrez
         private void colocarPecas()
         {
             colocarPecaNova('c', 1, new Torre(tab, Cor.Branca));
-            colocarPecaNova('c', 2, new Torre(tab, Cor.Branca));
-            colocarPecaNova('d', 2, new Torre(tab, Cor.Branca));
-            colocarPecaNova('e', 2, new Torre(tab, Cor.Branca));
-            colocarPecaNova('e', 1, new Torre(tab, Cor.Branca));
             colocarPecaNova('d', 1, new Rei(tab, Cor.Branca));
+            colocarPecaNova('h', 7, new Torre(tab, Cor.Branca));
 
-            colocarPecaNova('c', 7, new Torre(tab, Cor.Preta));
-            colocarPecaNova('c', 8, new Torre(tab, Cor.Preta));
-            colocarPecaNova('d', 7, new Torre(tab, Cor.Preta));
-            colocarPecaNova('e', 7, new Torre(tab, Cor.Preta));
-            colocarPecaNova('e', 8, new Torre(tab, Cor.Preta));
-            colocarPecaNova('d', 8, new Rei(tab, Cor.Preta));
+            colocarPecaNova('a', 8, new Rei(tab, Cor.Preta));
+            colocarPecaNova('b', 8, new Torre(tab, Cor.Preta));
         }
     }
 }
